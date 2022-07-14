5727be49eeefa6d71ab444572342963b827c9c18
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-07-14 12:51:26 +0300

    header is done

---
+++ src/Components/Header/index.js
@@ -0,0 +1,15 @@
+import './index.css'
+
+let Header = () => {
+
+    return <div className='header'>
+        <div className='header-left-side'>
+            Where in the world?
+        </div>
+        <div className='header-right-side'>
+            Dark Mode
+        </div>
+    </div>
+
+}
+export default Header;
---
+++ src/Components/MainWrapper/index.js
@@ -0,0 +1,11 @@
+import Header from '../Header'
+import './index.css'
+
+let MainWrapper = () =>{
+
+    return <div className='main-wrapper'>
+        <Header/>
+    </div>
+}
+
+export default MainWrapper
