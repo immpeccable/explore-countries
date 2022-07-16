2b1f17e5ed051662e46b557535a77af19a3707df
tunahan dündar <84476929+immpeccable@users.noreply.github.com>
2022-07-16 17:43:56 +0300

    mobile responsiveness
    mobile responsiveness for main page added

--- src/Components/Country/index.js
+++ src/Components/Country/index.js
@@ -4,7 +4,7 @@ export default function Country({mode, countryInfo}) {
   return (
     <div className={`country-wrapper ${mode === "white" ? "country-wrapper-light" : "country-wrapper-dark"}`}>
       <img className="country-flag" src={countryInfo.flags.png} alt="error" />
-      <div className="country-name">{countryInfo.name.official}</div>
+      <div className="country-namee">{countryInfo.name.official}</div>
       <div className="country-info">
         <div className="mid-text">Population: <span className="light-text">{countryInfo.population} </span> </div>
         <div className="mid-text">Region: <span className="light-text"> {countryInfo.region}</span></div>
